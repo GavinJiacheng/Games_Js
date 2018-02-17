1e3496b574b7ed660dda88c561fcd02519b0c86f
Gavin <ghsuxjc@gmail.com>
2018-02-17 04:07:34 -0800

    First commit

---
+++ js/myjs.js
@@ -0,0 +1,16 @@
+
+
+function playAudio() {
+    var x = document.getElementById("myAudio");
+    var y = document.getElementById("play");
+    if (x.paused)
+    {
+        x.play();
+        y.style.backgroundImage = 'url("img/pause.png")';
+    }
+    else
+    {
+        x.pause();
+        y.style.backgroundImage = 'url("img/play.png")';
+    }
+}
